e937a6eb334d9ef8c9ec575561ebd2dc2a7f2c02
Roman Abramenko <77733638+ElITEZONE@users.noreply.github.com>
2022-06-15 17:14:57 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
 
-    document.addEventListener('DOMContentLoaded', () => {
-        // This variable stores the bandwidth
+                // This variable stores the bandwidth
         // const bandWidth = navigator.connection.downlink;
         const screenWidth = window.screen.width;
 
         const video = document.querySelector('.video')
 
+        const mql = window.matchMedia("(orientation: portrait)");
+
+        if(mql.matches) {
+            alert(22)
+        } else {
             if(screenWidth <= 543) {
                 video.innerHTML = `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/694808160?h=f391fa9f59&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="GS_15SEC_REF.mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>`
             } else if (screenWidth >= 544 && screenWidth <= 767) {
@@ -21,9 +25,4 @@
                 video.innerHTML = `
                 <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/711137996?h=ed6894bffb&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="HELMET X1.mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>        `
             }
-
-        })
-
-window.addEventListener('orientationchange', () => {
-   alert('change')
-})
+        }
